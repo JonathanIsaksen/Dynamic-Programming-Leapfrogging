fdc8846c3242db95c7715212110481a5e7cd0e3a
Profz <jonathan.thor.isaksen@gmail.com>
2022-05-27 21:21:33 +0200

    addOne in rls should work now

--- RLS.py
+++ RLS.py
@@ -7,6 +7,9 @@ import scipy.optimize as optimize
 import scipy.sparse as sparse
 from quantecon import compute_fixed_point
 from quantecon.markov import DiscreteDP
+from Leapfrogging import leapfrogging
+lf = leapfrogging()
+import copy
 
 class rls_class:
     # def __init__(self):
@@ -14,8 +17,8 @@ class rls_class:
     def solve(self,G,ss,ESS0, stage_index):
 
         rlsp = {'maxEQ':np.NaN, 'print':np.NaN}
-        rlsp['maxEQ']  =  50000 # 50000 maximum number of iterations
-        rlsp['print']  =  10000   # 500 print every rlsp.print equilibria (0: no print, 1: print every, 2: print every second)
+        rlsp['maxEQ']  =  10 # 50000 maximum number of iterations
+        rlsp['print']  =  100  # 500 print every rlsp.print equilibria (0: no print, 1: print every, 2: print every second)
 
     #     % initialize matrices
         TAU = np.empty(rlsp['maxEQ'])
@@ -23,13 +26,12 @@ class rls_class:
         tau = np.size(stage_index) # start RLS at last stage
         iEQ = 0
         ESS = np.empty(rlsp['maxEQ'],dtype=object)
-        ESS[:] = ESS0
+        ESS[:] = copy.deepcopy(ESS0)
         out = np.empty(rlsp['maxEQ'])
         out[:] = np.NaN
         while iEQ < rlsp['maxEQ']:
             TAU[iEQ] = tau
             ss, ESS[iEQ] = G(ss,ESS[iEQ], tau)
-            iEQ += 1
 
             if (np.mod(iEQ,rlsp['print'])==0):
                 print(f'ESR[{iEQ}][\'esr\']  :')
@@ -40,11 +42,26 @@ class rls_class:
             # if nargout>2: # if there are less than 2 outputs xxx
             try:
                 np.insert(out,iEQ,self.output(ss, ESS[iEQ]))
+                print(f'we are in the try now at {iEQ} ')
             except:
-                continue
+                pass
+            print(iEQ)
+            ESS[iEQ+1] = self.addOne(copy.deepcopy(ESS[iEQ]))
+            temp_index = []
+            temp_index_true = []
+            for i in range(len(ESS[iEQ+1]['esr'])):
+                print(f'+1 is here: {ESS[iEQ+1]["esr"][i]}')
+                print(f'not +1 is here: {ESS[iEQ]["esr"][i]}')
+                temp_index_true.append((ESS[iEQ+1]['esr'][i]-ESS[iEQ]['esr'][i])!=0)
+            print(temp_index_true)
+            for i,j in enumerate(temp_index_true):
+                if j:
+                    temp_index.append(ESS[iEQ+1]['esr'][i])
+                else:
+                    temp_index.append(0)
+            print(temp_index)
 
-            ESS[iEQ+1] = self.addOne[ESS[iEQ]]
-            changeindex = min(np.nonzero((ESS[iEQ+1]['esr']-ESS[iEQ]['esr'])!=0))
+            changeindex = min(np.nonzero((ESS[iEQ+1]['esr'][0]-ESS[iEQ]['esr'][0])!=0))
             tau = sum(changeindex<=stage_index)-1 #% tau0 is found
 
             if np.all(ESS[iEQ+1]['esr']==-1):
@@ -56,31 +73,30 @@ class rls_class:
     
         return ESS, TAU, out
 
-    def output(ss, ESS):
+    def output(self,ss, ESS):
         out = {}
-        out['MPEesr'] = ESS.esr
+        out['MPEesr'] = ESS['esr']
         out['V1'] = max([ss[1]['EQs'][1,1,1]['eq']['vN1'],ss[1]['EQs'][1,1,1]['eq']['vI1']]) #  xxx perhabs change ss[1] to 0
         out['V2'] = max([ss[1]['EQs'][1,1,1]['eq']['vN2'],ss[1]['EQs'][1,1,1]['eq']['vI2']]) #  xxx perhabs change ss[1] to 0
         return out 
 
-    def addOne(ESS):
+    def addOne(self,addESS):
         # %if x[1,1,1] == -1
         # %    throw(error("This ESS has already overflown!"))
         # %end
-        n = len(ESS['esr'])   
+        n = len(addESS['esr'][0])
         X = np.zeros(n)
         R = 1
         for i in np.flip(np.arange(n)):
-            X[i] = np.mod(ESS['esr'][i] + R,ESS['bases'][i])
+            X[i] = int(np.mod(addESS['esr'][0,i] + R,addESS['bases'][0,i]+1)) # xxx added 1 as to not divide by 0
             # % mod(a,b) does division and returns the remainder given as a-div(a,b)*b
-            R = lf.div(ESS['esr'][i] + R,ESS['bases'][i])
+            R = lf.div(addESS['esr'][0,i] + R,addESS['bases'][0,i]+1)
             # % div(a,b) does division and truncates - rounding down - to nearest integer  .... floor(a/b)
         if R > 0:
         # % When exiting the loop R > 0 occurs when all ESS.number is max allowed
         # % which is 1 below the base.
         # % println("No more equilibria to check.")
-            ESS['esr'] = -1*np.ones(n)
+            addESS['esr'] = -1*np.ones(n)
         else:
-            ESS['esr'] = X
-
-        return ESS
+            addESS['esr'] = X
+        return addESS
